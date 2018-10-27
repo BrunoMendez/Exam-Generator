c576fe25e3d24fe9128de7e38a0e4578ff62849c
BrunoMendez <bruno.m.275@gmail.com>
2018-10-27 14:08:08 -0500

    navigation

--- src/controllers/GenerarExamenController.java
+++ src/controllers/GenerarExamenController.java
@@ -5,12 +5,18 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,8 +24,11 @@ import javafx.scene.control.Label;
  */
 public class GenerarExamenController implements Initializable {
     
+    public Stage stage = new Stage();
     @FXML
     private Label label;
+    @FXML
+    private Button returnButton;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -29,6 +38,17 @@ public class GenerarExamenController implements Initializable {
     public final void DropMenu(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Mate");
+    }
+    
+    @FXML
+    private void goBack(ActionEvent event) 
+        throws IOException
+    {
+        this.stage = ((Stage)this.returnButton.getScene().getWindow());
+        Parent root = (Parent)FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+        Scene scene = new Scene(root);
+        this.stage.setScene(scene);
+        this.stage.show();
     }
     
     @Override
